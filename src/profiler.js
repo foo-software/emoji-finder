a8c31fdd6636277004bb3c4d84826322542e7704
Adam Henson <adamhenson1979@gmail.com>
2019-09-08 17:27:11 -0400

    cumulative time

--- src/profiler.js
+++ src/profiler.js
@@ -1,3 +1,6 @@
+// the entrire render time since execution of this file (likely on page load)
+let cumulativeDuration = 0;
+
 // profiler callback
 // https://reactjs.org/docs/profiler.html#onrender-callback
 export const getProfileData = ([
@@ -8,21 +11,31 @@ export const getProfileData = ([
   startTime, // when React began rendering this update
   commitTime, // when React committed this update
   interactions // the Set of interactions belonging to this update
-]) => ({
-  id,
-  interactions,
-  phase,
-  actualDuration: Number(actualDuration.toFixed(2)),
-  baseDuration: Number(baseDuration.toFixed(2)),
-  commitTime: Number(commitTime.toFixed(2)),
-  startTime: Number(startTime.toFixed(2))
-});
+]) => {
+  cumulativeDuration = Number((cumulativeDuration + actualDuration).toFixed(2));
+  return {
+    id,
+    interactions,
+    phase,
+    actualDuration: Number(actualDuration.toFixed(2)),
+    baseDuration: Number(baseDuration.toFixed(2)),
+    commitTime: Number(commitTime.toFixed(2)),
+    cumulativeDuration,
+    startTime: Number(startTime.toFixed(2))
+  };
+};
 
-export const logProfileData = ({ actualDuration, baseDuration, phase }) => {
+export const logProfileData = ({
+  actualDuration,
+  baseDuration,
+  cumulativeDuration,
+  phase
+}) => {
   console.group(phase);
   console.table({
     actualDuration,
-    baseDuration
+    baseDuration,
+    cumulativeDuration
   });
   console.groupEnd();
 };
